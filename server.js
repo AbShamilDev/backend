1fbec1333fd34f7fdf651b0b346db81240ac4418
AbShamilDev <abkarov02@gmail.com>
2024-07-26 19:07:49 +0300

    feat: getAll fnc restore

--- server.js
+++ server.js
@@ -12,6 +12,28 @@ const port = 8080;
 
 app.use(cors({ headers: ["Content-Type"] }));
 
+app.get("/getall", async (req, res) => {
+  console.log("Get all!");
+  const data = {
+    installations: await connection.query("SELECT * FROM `Installations`"),
+    projects: await connection.query("SELECT * FROM `Projects`"),
+    specifications: await connection.query("SELECT * FROM `Specifications`"),
+    components: await connection.query("SELECT * from Components"),
+    departments: await connection.query("SELECT * FROM `Departments`"),
+    units: await connection.query("SELECT * FROM `Units`"),
+    clients: await connection.query("SELECT * FROM `Clients`"),
+  };
+  data.installations = data.installations[0];
+  data.projects = data.projects[0];
+  data.specifications = data.specifications[0];
+  data.components = data.components[0];
+  data.departments = data.departments[0];
+  data.units = data.units[0];
+  data.clients = data.clients[0];
+
+  res.send(data);
+});
+
 app.use("/installations", installationsRouter);
 app.use("/components", componentsRouter);
 app.use("/projects", projectsRouter);
